5f9ce2b6cec736aec3013503a4ead95c5537132a
Ronin Sharma <rrs234@cornell.edu>
2022-04-03 18:34:07 -0400

    add more filters

--- web-app/src/HomePage.jsx
+++ web-app/src/HomePage.jsx
@@ -197,6 +197,8 @@ export default class HomePage extends Component {
                         <input type="checkbox" name="check-1" value="check-1" id="check-1" />
                         <label for="check-1">Department</label>
                         <input id='department-text' type="text"></input>
+                        <br></br>
+                        <br></br>
                     </div>
 
                     <div>
@@ -230,12 +232,80 @@ export default class HomePage extends Component {
                         <input type="checkbox" name="check-med-3" value="check-med-6" class="check-med" />
                         <label for="check-med-6">B-</label>
 
+                        <br></br>
+                        <br></br>
                     </div>
 
                     <div>
                         <input type="checkbox" name="check-4" value="check-4" id="check-4" />
-                        <label for="check-4"></label>
+                        <label for="check-4">Course Number</label>
+                        <input id='course-num-text' type="text"></input>
+                        <br></br>
+                        <br></br>
+                    </div>
 
+                    <div>
+                        <input type="checkbox" name="check-5" value="check-5" id="check-5" />
+                        <label for="check-5">Course Name</label>
+                        <input id='course-name-text' type="text"></input>
+                        <br></br>
+                        <br></br>
+                    </div>
+
+                    <div>
+                        <input type="checkbox" name="check-6" value="check-6" id="check-6" />
+                        <label for="check-6">Professor Difficulty</label>
+                        <input id='prof-diff-text' type="text"></input>
+                        <br></br>
+                        <br></br>
+                    </div>
+
+                    <div>
+                        <input type="checkbox" name="check-7" value="check-7" id="check-7" />
+                        <label for="check-7">Class Difficulty</label>
+                        <input id='class-diff-text' type="text"></input>
+                        <br></br>
+                        <br></br>
+                    </div>
+
+                    <div>
+                        <input type="checkbox" name="check-8" value="check-8" id="check-8" />
+                        <label for="check-8">Class Rating</label>
+                        <input id='class-rat-text' type="text"></input>
+                        <br></br>
+                        <br></br>
+                    </div>
+
+                    <div>
+                        <input type="checkbox" name="check-9" value="check-9" id="check-9" />
+                        <label for="check-9">Class Workload</label>
+                        <input id='class-work-text' type="text"></input>
+                        <br></br>
+                        <br></br>
+                    </div>
+
+                    <div>
+                        <input type="checkbox" name="check-10" value="check-10" id="check-10" />
+                        <label for="check-10">Start Time</label>
+                        <input id='start-time-text' type="text"></input>
+                        <br></br>
+                        <br></br>
+                    </div>
+
+                    <div>
+                        <input type="checkbox" name="check-11" value="check-11" id="check-11" />
+                        <label for="check-11">End Time</label>
+                        <input id='end-time-text' type="text"></input>
+                        <br></br>
+                        <br></br>
+                    </div>
+
+                    <div>
+                        <input type="checkbox" name="check-11" value="check-11" id="check-11" />
+                        <label for="check-11">Location</label>
+                        <input id='location-text' type="text"></input>
+                        <br></br>
+                        <br></br>
                     </div>
 
                     <br></br>
@@ -261,21 +331,10 @@ export default class HomePage extends Component {
                     
                     <table id="class-info-table">
                         <thead id="class-info-header">
-                            <tr id="class-info-header-row">
-                                {/* <th>Course #</th>
-                                <th>Class</th>
-                                <th>Professor</th>
-                                <th>Semester</th> */}
-                            </tr>
+                            <tr id="class-info-header-row"></tr>
                         </thead>
-
                         <tbody></tbody>
                         
-                        {/* <tr>
-                            <td>1</td>
-                            <td>2</td>
-                            <td>3</td>
-                        </tr> */}
                     </table>
 
                     {list}
